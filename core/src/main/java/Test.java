11df0868b6181292538f9787ccc3920171194f8a
david lee <davidlee@daviddeMacBook-Pro.local>
2018-09-10 21:45:50 +0800

    add：测试

---
+++ core/src/main/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by davidlee on 2018/9/10.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("---OK");
+    }
+}
---
+++ core/src/test/java/TestCode.java
@@ -0,0 +1,8 @@
+/**
+ * Created by davidlee on 2018/9/10.
+ */
+public class TestCode {
+    public static void main(String[] args) {
+        System.out.println("---Test");
+    }
+}
